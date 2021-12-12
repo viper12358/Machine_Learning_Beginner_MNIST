5362a9de500d619434b2c2817b03fa6e02bfee48
Adrian Tran <38680352+viper12358@users.noreply.github.com>
2021-12-12 05:11:43 -0700

    update heart_starter and binary, 72% reached

--- heart_starter.py
+++ heart_starter.py
@@ -1,7 +1,7 @@
 import csv
 import numpy as np
 import network
-
+import time
 
 # converts a 1d python list into a (1,n) row vector
 def rv(vec):
@@ -23,6 +23,62 @@ def standardize(x, mu, sigma):
     
 
 ##############################################
+
+def getDataFromSample(sample):
+    # [1]               min		max		average		std.dev
+    # sbp				101		218		138.3		20.5
+    sbp_mean = 138.3
+    sbp_stdDev = 20.5
+    sbp = cv([standardize(float(sample[1]), sbp_mean, sbp_stdDev)])
+    
+    # tobacco			0		31.2	3.64		4.59        [2]
+    tobacco_mean = 3.64
+    tobacco_stdDev = 4.59
+    tobacco = cv([standardize(float(sample[2]), tobacco_mean, tobacco_stdDev)])
+    
+    # ldl				.98		15.33	4.74		2.07        [3]
+    ldl_mean = 4.74
+    ldl_stdDev = 2.07
+    ldl = cv([standardize(float(sample[3]), ldl_mean, ldl_stdDev)])
+    
+    # adiposity		   6.74	    42.49   25.4		7.77        [4]
+    adiposity_mean = 25.4
+    adiposity_stdDev = 7.77
+    adiposity = cv([standardize(float(sample[4]), adiposity_mean, adiposity_stdDev)])
+    
+    # famhist			binary value (1 = Present, 0 = Absent)  [5]
+    if (sample[5] == "Present"):
+        famhist = cv([1])    
+    elif (sample[5] == "Absent"):
+        famhist = cv([0])
+    else:
+        print("Data processing error. Exiting program.")
+        quit()
+    
+    # typea			    13		78		53.1		9.81        [6]
+    typea_mean = 53.1
+    typea_stdDev = 9.81
+    typea = cv([standardize(float(sample[6]), typea_mean, typea_stdDev)])
+    
+    # obesity			14.7	46.58	26.0		4.21        [7]
+    obesity_mean = 53.1
+    obesity_stdDev = 9.81
+    obesity = cv([standardize(float(sample[7]), obesity_mean, obesity_stdDev)])
+    
+    # alcohol			0		147.19	17.0		24.5        [8]
+    alcohol_mean = 17.0
+    alcohol_stdDev = 24.5
+    alcohol = cv([standardize(float(sample[8]), alcohol_mean, alcohol_stdDev)])
+    
+    # age				15		64		42.8		14.6	    [9]
+    age_int = int(sample[9])
+    age = cv([float(age_int/64.0)])
+    
+    features = np.concatenate((sbp, tobacco, ldl, adiposity, famhist, typea, obesity, alcohol, age), axis=0)
+    
+    label = int(sample[10])
+    
+    return features, label
 
 # reads number of data points, feature vectors and their labels from the given file
 # and returns them as a tuple
@@ -59,17 +115,34 @@ def prepData():
     n, features, labels = readData('data/heart.csv')
 
     # CODE GOES HERE
+    ntrain = int(n * 5/6)    
+    ntest = n - ntrain
+    trainingFeatures = features[:ntrain]
+    trainingLabels = [onehot(label, 2) for label in labels[:ntrain]]    # training labels should be in onehot form
 
+    print(f"Number of training samples: {ntrain}")
+
+    testingFeatures = features[ntrain:]
+    testingLabels = labels[ntrain:]
+    print(f"Number of testing samples: {ntest}")
+
+    trainingData = zip(trainingFeatures, trainingLabels)
+    testingData = zip(testingFeatures, testingLabels)
     return (trainingData, testingData)
+
 
 
 ###################################################
 
-
+start = time.time()
 trainingData, testingData = prepData()
 
-net = network.Network([9,10,2])
-net.SGD(trainingData, 10, 10, .1, test_data = testingData)
+net = network.Network([9,10,5,2])
+net.SGD(trainingData, 8, 5, 0.2, test_data = testingData)
+
+end = time.time()
+network.saveToFile(net, "part3.pkl")
+print("Time taken in seconds: ", end-start)
 
 
        
